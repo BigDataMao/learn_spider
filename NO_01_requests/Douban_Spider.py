c659a55b9a1056a8344442b1d3203f8a63985f6f
flinkmao <mxw712@gmail.com>
2023-03-16 20:38:31 +0800

    豆瓣框架

---
+++ NO_01_requests/Douban_Spider.py
@@ -0,0 +1,18 @@
+# 导包
+import requests
+import lxml
+
+# base_url,parse,headers
+base_url = 'https://movie.douban.com/top250'
+headers = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36 Edg/110.0.1587.57'
+}
+
+# xpath模块解码
+
+# 打印一页尝试
+
+# 更改多页
+
+# 更改进入数据库
+
